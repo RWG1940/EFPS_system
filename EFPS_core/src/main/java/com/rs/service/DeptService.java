8e6028737e0b0af58c45378520d31031e8acb996
RWG <1940449715@qq.com>
2024-08-02 13:13:50 +0800

    EFPS系统

---
+++ EFPS_core/src/main/java/com/rs/service/DeptService.java
@@ -0,0 +1,13 @@
+package com.rs.service;
+
+import com.rs.domain.Dept;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author RWG
+* @description 针对表【dept】的数据库操作Service
+* @createDate 2024-07-28 12:03:30
+*/
+public interface DeptService extends IService<Dept> {
+
+}
---
+++ EFPS_core/src/main/java/com/rs/service/impl/DeptServiceImpl.java
@@ -0,0 +1,22 @@
+package com.rs.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.rs.domain.Dept;
+import com.rs.service.DeptService;
+import com.rs.mapper.DeptMapper;
+import org.springframework.stereotype.Service;
+
+/**
+* @author RWG
+* @description 针对表【dept】的数据库操作Service实现
+* @createDate 2024-07-28 12:03:30
+*/
+@Service
+public class DeptServiceImpl extends ServiceImpl<DeptMapper, Dept>
+    implements DeptService{
+
+}
+
+
+
+
